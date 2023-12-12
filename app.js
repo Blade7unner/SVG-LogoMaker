767f429da0e060a63e8c0b4e8875d9c960699def
Benoit <fbenoit2077@gmail.com>
2023-12-11 21:43:17 -0500

    node test, function createsvg

--- app.js
+++ app.js
@@ -37,3 +37,30 @@ function promptUser() {
         }
     ]);
 }
+
+function createSvg({ text, textColor, shape, shapeColor }) {
+    let svgString = '<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+
+    if (shape === 'circle') {
+        svgString += `<circle cx="150" cy="100" r="80" fill="${shapeColor}" />`;
+    } else if (shape === 'triangle') {
+        svgString += `<polygon points="150,20 230,180 70,180" fill="${shapeColor}" />`;
+    } else if (shape === 'square') {
+        svgString += `<rect x="70" y="60" width="160" height="160" fill="${shapeColor}" />`;
+    }
+
+    svgString += `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`;
+    svgString += '</svg>';
+
+    return svgString;
+}
+
+function main() {
+    promptUser().then(answers => {
+        const svgContent = createSvg(answers);
+        fs.writeFileSync('logo.svg', svgContent);
+        console.log('Generated logo.svg');
+    });
+}
+
+main();
