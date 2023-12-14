ddd847ca06316b2254e40ee9e0df4ce75ff9b760
Benoit <fbenoit2077@gmail.com>
2023-12-13 23:23:58 -0500

    tests and jestconfig

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+export default {
+    transform: {},
+    testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'mjs', 'json', 'node'],
+    testMatch: [
+      "**/__tests__/**/*.[jt]s?(x)",
+      "**/?(*.)+(spec|test).[jt]s?(x)",
+      "**/?(*.)+(spec|test).mjs"
+    ],
+  };
